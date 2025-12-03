c8cf1a76491648648d87a1dc625ee97d546955a2
Jonathan Chen <jonafish10@gmail.com>
2025-12-03 17:36:39 -0500

    [Day 3] Update cloc

--- cloc.sh
+++ cloc.sh
@@ -6,11 +6,13 @@ SCRIPT_DIR="$(pwd)"
 
 echo "============="
 echo "LINES OF CODE"
+echo "============="
+echo "TEMPLATE   $(wc -l < "$SCRIPT_DIR/template.py" | tr -d '[:space:]')"
 for i in {1..12}
 do
   SOLN_FILE="$SCRIPT_DIR/day_${i}/soln.py"
   if [ -e "$SOLN_FILE" ]; then
-    echo "Day $i    $(wc -l < $SOLN_FILE | tr -d '[:space:]')"
+    wc -l < $SOLN_FILE | tr -d '[:space:]' | xargs printf "Day $i     %3s\n"
   else
     break
   fi
