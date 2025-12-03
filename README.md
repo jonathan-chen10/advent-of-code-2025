d1fdd064a57674eebd4ff0ee33de3be285658010
Jonathan Chen <jonafish10@gmail.com>
2025-12-03 00:38:34 -0500

    [Day 2] Solve part 2

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Solutions to Advent of Code 2025, in Python 3.14.
 
 My only goal is to finish the Advent of Code, i.e. do one problem every day. With 12 days it should be easier.
 
-| Day                      | Part 1                                                | Part 2                    |
-| ------------------------ | ----------------------------------------------------- | ------------------------- |
-| [Day 1](./day_1/soln.py) | Addition mod 100                                      | Per-step Integer division |
-| [Day 2](./day_2/soln.py) | Calculate first and last qualifying numbers per range | Per-step Integer division |
+| Day                      | Part 1                                       | Part 2                                   |
+| ------------------------ | -------------------------------------------- | ---------------------------------------- |
+| [Day 1](./day_1/soln.py) | Addition mod 100                             | Per-step Integer division                |
+| [Day 2](./day_2/soln.py) | Find first and last repeating nums per range | Repeat part 1 for all sizes, remove dups |
--- day_2/soln.py
+++ day_2/soln.py
@@ -10,36 +10,53 @@ def solve_A(input_lines: list) -> int:
   sum = 0
   for r in ranges:
     start_s, end_s = r.split('-')
-    first_pre = next_pre_s(start_s)
-    last_pre = prev_pre_s(end_s)
+    first_pre = next_pre_s(start_s, 2)
+    last_pre = prev_pre_s(end_s, 2)
     for n in range(int(first_pre), int(last_pre) + 1):
       sum += int(str(n) * 2)
   return sum 
 
-def next_pre_s(sn: str) -> str:
-  if len(sn) % 2 != 0:
+def next_pre_s(sn: str, num_reps) -> str:
+  if len(sn) % num_reps != 0:
     # smallest repeating number with d+1 digits
-    return '1' + '0' * (len(sn) // 2)
-  candidate_half_s = sn[:len(sn) // 2]
-  candidate = int(candidate_half_s + candidate_half_s)
+    return '1' + '0' * (len(sn) // num_reps)
+  candidate_atom_s = sn[:len(sn) // num_reps]
+  candidate = int(candidate_atom_s * num_reps)
   if candidate >= int(sn):
-    return candidate_half_s
+    return candidate_atom_s
   else:
-    return str(int(candidate_half_s) + 1)
+    return str(int(candidate_atom_s) + 1)
 
-def prev_pre_s(sn: str) -> str:
-  if len(sn) % 2 != 0:
+def prev_pre_s(sn: str, num_reps) -> str:
+  if len(sn) % num_reps != 0:
     # largest repeating number with d-1 digits
-    return '9' * (len(sn) // 2)
-  candidate_half_s = sn[:len(sn) // 2]
-  candidate = int(candidate_half_s + candidate_half_s)
+    return '9' * (len(sn) // num_reps)
+  candidate_atom_s = sn[:len(sn) // num_reps]
+  candidate = int(candidate_atom_s * num_reps)
   if candidate <= int(sn):
-    return candidate_half_s
+    return candidate_atom_s
   else:
-    return str(int(candidate_half_s) - 1)
+    return str(int(candidate_atom_s) - 1)
 
 def solve_B(input_lines: list):
-  pass
+  ranges = input_lines[0].split(',') 
+  sum = 0
+  for r in ranges:
+    start_s, end_s = r.split('-')
+    for num_reps in range(2, len(end_s) + 1):
+      first_pre = next_pre_s(start_s, num_reps)
+      last_pre = prev_pre_s(end_s, num_reps)
+      for n in range(int(first_pre), int(last_pre) + 1):
+        if not repeating_num(n):
+          sum += int(str(n) * num_reps)
+  return sum 
+
+def repeating_num(n: int) -> bool:
+  sn = str(n)
+  for num_reps in range(2, len(sn) + 1):
+    if sn == sn[:len(sn) // num_reps] * num_reps:
+      return True
+  return False
 
 if __name__ == '__main__':
   argparser = argparse.ArgumentParser()
