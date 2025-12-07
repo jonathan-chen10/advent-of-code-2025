4832b22b0fdfd82019aeff947d1040b599f953e3
Jonathan Chen <jonafish10@gmail.com>
2025-12-07 16:36:17 -0500

    [Day 5] Solve part 2

--- README.md
+++ README.md
@@ -10,4 +10,4 @@ My only goal is to finish the Advent of Code, i.e. do one problem every day. Wit
 | [Day 2](./day_2/soln.py) | Find first and last repeating nums per range | Repeat part 1 for all sizes, remove dups |
 | [Day 3](./day_3/soln.py) | Greedily pick each digit                     | Same as part 1                           |
 | [Day 4](./day_4/soln.py) | Iterate through all locations/neighbors      | Iteratively remove until fixed point     |
-| [Day 5](./day_5/soln.py) | Linear scan of ranges                        | Iteratively remove until fixed point     |
+| [Day 5](./day_5/soln.py) | Linear scan of ranges                        | Merge intervals and add widths           |
--- day_5/soln.py
+++ day_5/soln.py
@@ -23,8 +23,25 @@ class RangeSet():
   
   def all_true(self, pred: Callable[[tuple[int, int]], bool]) -> bool:
     return all(pred(r) for r in self.ranges)
-
   
+  def has_num(self, query: int) -> bool:
+    return self.any_true(lambda ran : ran[0] <= query <= ran[1])
+  
+  def ranges_disjoint(self) -> list[tuple[int, int]]:
+    res = []
+    for ran in sorted(self.ranges, key = lambda r : r[0]):
+      if len(res) == 0:
+        res.append(ran)
+      else:
+        last = res[-1]
+        if last[1] < ran[0] - 1:
+          res.append(ran)
+        else:
+          res[-1] = (last[0], max(last[1], ran[1]))
+    return res
+  
+  def total_length(self) -> int:
+    return sum(r[1] - r[0] + 1 for r in self.ranges_disjoint())  
 
 def solve_A(input_lines: list[str]) -> int:
   ranges = RangeSet()
@@ -36,12 +53,19 @@ def solve_A(input_lines: list[str]) -> int:
     elif len(line.strip()) > 0:
       ingredients.append(int(line))
   
-  return sum(1 for id in ingredients 
-             if ranges.any_true(lambda ran : ran[0] <= id <= ran[1]))
+  return sum(1 for id in ingredients if ranges.has_num(id))
 
 
 def solve_B(input_lines: list[str]) -> int:
-  raise NotImplementedError()
+  ranges = RangeSet()
+  for line in input_lines:
+    if '-' in line:
+      start, end = [int(n) for n in line.split('-')]
+      ranges.add_range(start, end)
+    if len(line.strip()) == 0:
+      break
+  
+  return ranges.total_length()
 
 if __name__ == '__main__':
   main_day(5)
